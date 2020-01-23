da8b95552552e7cce7c5edbb342888db0c733239
Oldrich Havelka <oldrichhavelka80085@gmail.com>
2020-01-23 16:00:15 +0100

    Very commited. Truly in love.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,20 @@ package sample;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.FileChooser;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import java.awt.*;
 import javax.imageio.ImageIO;
@@ -36,9 +44,25 @@ public class Controller {
     private Image modifiedImage;
 
     @FXML
+    private Label tresholdvalueLabel;
+
+    @FXML
+    private Slider tresholdvalueSlider;
+
+    @FXML
+    private Button tresholdvalueButton;
+
+    @FXML
     void exit(ActionEvent event) {
         Stage stage = (Stage) root.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void initialize(){
+        tresholdvalueSlider.setMax(255);
+        tresholdvalueSlider.setMin(0);
+        tresholdvalueSlider.setValue(128);
     }
 
     @FXML
@@ -118,11 +142,36 @@ public class Controller {
     }
 
     @FXML
+    private void tresholdFlag(){
+        filterFlag = "treshold";
+    }
+
+    @FXML
     private void aplikujFiltr(){
         switch (filterFlag){
             case "negative": vynegujObrazek(); break;
+            case "treshold":
+                tresholdvalueLabel.setVisible(true);
+                tresholdvalueSlider.setVisible(true);
+                tresholdvalueButton.setVisible(true);
+                break;
         }
     }
+
+    @FXML
+    private void getAutomaticTreshold(){
+        BufferedImage img = SwingFXUtils.fromFXImage(originalImage, null);
+        double output = 0;
+        for (int x = 0; x < img.getWidth(); x++) {
+            for (int y = 0; y < img.getWidth(); y++) {
+                Color c = new Color(img.getRGB(x,y));
+                output+=c.getRed()+c.getGreen()+c.getBlue();
+            }
+        }
+        output = output / (img.getWidth()*img.getHeight()*3);
+        tresholdvalueSlider.setValue((int)output);
+    }
+
 
     @FXML
     private void vynegujObrazek(){
@@ -146,6 +195,28 @@ public class Controller {
             System.out.println("Image not selected or something else and I hope it's not the something else option.");
         }
 
+    }
+
+    @FXML
+    private void tresholduj(){
+        int black = Color.BLACK.getRGB();
+        int white = Color.WHITE.getRed();
+        BufferedImage img = SwingFXUtils.fromFXImage(originalImage, null);
+        for (int x = 0; x < img.getWidth(); x++) {
+            for (int y = 0; y < img.getWidth(); y++) {
+                int rgb = img.getRGB(x,y);
+                Color c = new Color(rgb);
+                if(c.getRed() > tresholdvalueSlider.getValue() || c.getBlue() > tresholdvalueSlider.getValue() || c.getGreen() > tresholdvalueSlider.getValue()){
+                    img.setRGB(x,y,white);
+                }
+                else
+                {
+                    img.setRGB(x,y,black);
+                }
+            }
+        }
+        imageViewBig.setImage(SwingFXUtils.toFXImage(img, null));
+        modifiedImage = SwingFXUtils.toFXImage(img, null);
     }
 
     @FXML
